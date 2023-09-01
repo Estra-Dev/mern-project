ef3374215e3578b4b585ac504bbe46b8b20e6cbe
Estra-Dev <dominikolyson@gmail.com>
2023-09-01 20:05:48 +0100

    initial commit

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import bodyParser from "body-parser"
 import cors from "cors"
 import router from "./route/post.js"
+import "dotenv/config"
 
 const app = express()
 
@@ -19,4 +20,5 @@ app.all("*", (req, res) => res.send("This route does not exist"))
 
 app.listen(PORT, () => {
   console.log(`server is currently running on port ${PORT}`)
+  console.log(process.env)
 })
