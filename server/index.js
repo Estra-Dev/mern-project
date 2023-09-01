24d00be4d0509b573d05876f54935cf450892d8a
Estra-Dev <dominikolyson@gmail.com>
2023-09-01 16:18:37 +0100

    initial commit

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from "express"
+import bodyParser from "body-parser"
+import cors from "cors"
+import router from "./route/post.js"
+
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(cors())
+app.use("/", router)
+
+const PORT = 4000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Express JS")
+})
+app.all("*", (req, res) => res.send("This route does not exist"))
+
+app.listen(PORT, () => {
+  console.log(`server is currently running on port ${PORT}`)
+})
